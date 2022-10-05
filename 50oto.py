31352e2a207251e0e760eec000544dbeea3f1c54
Yuxuan <yyan@duck.com>
2022-10-05 14:33:23 +0800

    50oto

---
+++ 50oto.py
@@ -0,0 +1,30 @@
+import random
+import math
+
+alphabeta = [
+    'a', 'i', 'u', 'e', 'o',
+    'ka', 'ki', 'ku', 'ke', 'ko',
+    'ga', 'gi', 'gu', 'ge', 'go',
+    'sa', 'shi', 'su', 'se', 'so',
+    'za', 'ji', 'zu', 'ze', 'zo',
+    'ta', 'chi', 'tsu', 'te', 'to',
+    'da', 'ji', 'zu', 'de', 'do',
+    'na', 'ni', 'nu', 'ne', 'no',
+    'ha', 'hi', 'fu', 'he', 'ho',
+    'ba', 'bi', 'bu', 'be', 'bo',
+    'pa', 'pi', 'pu', 'pe', 'po',
+    'ma', 'mi', 'mu', 'me', 'mo',
+    'ya', 'yu', 'yo',
+    'ra', 'ri', 'ru', 're', 'ro',
+    'wa', 'wo',
+    'n']
+
+def main():
+    random.shuffle(alphabeta)
+    lw = 10
+    nlines = math.ceil(len(alphabeta) / lw)
+    for i in range(nlines):
+        print('\t'.join(alphabeta[lw*i:min([lw*(i+1),len(alphabeta)])]))
+
+if __name__ == '__main__':
+    main()
